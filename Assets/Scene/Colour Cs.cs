563de1c3c5ad731adc50646df16ae7c990726fe2
Tanu soni <Tanusoni2312@gmail.com>
2022-11-05 03:21:50 +0530

    effects

---
+++ Assets/Scene/Colour Cs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourCs : MonoBehaviour
+{
+   [SerializeField] private Material myMaterial;
+   private void onTriggerEnter(Collider other){
+    if(other.CompareTag("Player")){
+        myMaterial.color = Color.gr;
+    }
+   }
+   private void onTriggerExit(Collider other){
+    if(other.CompareTag("Player")){
+        myMaterial.Color = Color.red;
+    }
+   }
+}
